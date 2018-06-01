cdc38b4f11c148f899364cba6d5c2c5d160977e4
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-06-01 12:35:03 +0200

    Add models

---
+++ packages/backend/src/models/Urls.js
@@ -0,0 +1,34 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('Urls', {
+    id: {
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    url: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: {
+          args: [10, 200],
+          msg: 'Please provide field within 12 to 200 characters.',
+        },
+      },
+    },
+    hash: {
+      type: DataTypes.STRING(5),
+    },
+    visit: {
+      type: DataTypes.INTEGER,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  }, {
+    tableName: 'Urls',
+  })
+}
