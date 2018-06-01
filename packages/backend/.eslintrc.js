b7d627c13a291d693b672192f6dc52d5b0d2b880
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-06-01 10:15:51 +0200

    Setup backend repo

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "standard",
+    // add your custom rules here
+    rules: {
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'comma-dangle': [ 'error', 'always-multiline' ],
+      'space-before-function-paren': [ 'warn', {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'ignore',
+      } ],
+    }
+};
