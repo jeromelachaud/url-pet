d97305a8475e113025f411bb79859b40378b8da7
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-06-05 13:43:00 +0200

    Update config file

--- packages/backend/src/config/config.js
+++ packages/backend/src/config/config.js
@@ -1,17 +1,17 @@
-const path = require('path')
 const Sequelize = require('sequelize')
 
 module.exports = {
   host: process.env.APP_HOST,
   port: process.env.APP_PORT,
   db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     options: {
       dialect: process.env.DB_DIALECT,
       host: process.env.DB_HOST,
-      storage: path.resolve(__dirname, '../url-minifier.sqlite3'),
       operatorsAliases: {$and: Sequelize.Op.and},
     },
   },
