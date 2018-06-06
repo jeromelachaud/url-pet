08f8167102fa375c5b4522840579333eae0bbcc6
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-06-06 19:13:25 +0200

    Update config file

--- packages/backend/src/config/config.js
+++ packages/backend/src/config/config.js
@@ -1,24 +1,30 @@
 const Sequelize = require('sequelize')
 
 module.exports = {
-  host: process.env.APP_HOST,
-  port: process.env.APP_PORT,
+  host: 'localhost',
+  port: process.env.PORT || 8081,
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    host: 'urlminifieraws.c1v0bkjlnxr4.eu-west-1.rds.amazonaws.com',
+    port: 5432,
+    database: 'urlminifieraws',
+    user: 'jerome',
+    password: '1fdaj6im',
     options: {
-      dialect: process.env.DB_DIALECT,
-      host: process.env.DB_HOST,
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 20000,
+        acquire: 20000,
+      },
+      dialect: 'postgres',
+      host: 'urlminifieraws.c1v0bkjlnxr4.eu-west-1.rds.amazonaws.com',
       operatorsAliases: {$and: Sequelize.Op.and},
     },
   },
   basicAuth: {
     credentials: {
-      name: process.env.BASIC_AUTH_NAME,
-      pass: process.env.BASIC_AUTH_PASS,
+      name: 'jerome',
+      pass: 'admin',
     },
   },
   alphabet: {
