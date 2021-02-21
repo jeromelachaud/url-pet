020c46bc9eeb5c96f7f1c370b164613141c6ebe2
Jerome Lachaud <jerome.lachaud@gmail.com>
2021-02-21 17:54:48 +0100

    Fix Sequelize import

--- api/models/index.js
+++ api/models/index.js
@@ -17,7 +17,7 @@ if (process.env.DATABASE_URL) {
 fs.readdirSync(__dirname)
   .filter(file => file !== 'index.js')
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
   })
 db.sequelize = sequelize
