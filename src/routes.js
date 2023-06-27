b32f88a614ca5d989b09a7cd002c24b0ee5ef2ca
Jerome Lachaud <jerome.lachaud@gmail.com>
2023-06-27 21:53:58 +0200

    Update routing/templating

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Redirect, Route, Switch } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import { Admin } from './Pages/Admin'
 import { Home } from './Pages/Home'
 import { Login } from './Pages/Login'
 
 export default (
-  <Switch>
+  <Routes>
     <Route exact path="/login" component={Login} />
     <Route exact path="/admin" component={Admin} />
     <Route exact path="/" component={Home} />
-    <Redirect to="/" />
-  </Switch>
+    <Navigate to="/" />
+  </Routes>
 )
